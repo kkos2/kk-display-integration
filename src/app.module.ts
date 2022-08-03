083b0aa5041d7e7dc90e3e0d229fc4c50a7c9865
Mads H. Danquah <mads@danquah.dk>
2022-08-03 12:13:23 +0200

    Add prettier to eslint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
+
+
 import { TasksModule } from "./tasks/tasks.module";
 
 @Module({
   imports: [
+
     ScheduleModule.forRoot(),
     TasksModule
   ],
