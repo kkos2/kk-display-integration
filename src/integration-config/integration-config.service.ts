704bf2385fe44ed55aa56158e9a3b01ce6cb90d0
Mads H. Danquah <mads@danquah.dk>
2022-08-12 07:39:56 +0200

    Add an utility function to make error-messaging a bit better.

--- src/integration-config/integration-config.service.ts
+++ src/integration-config/integration-config.service.ts
@@ -3,20 +3,34 @@ import { ConfigService } from "@nestjs/config";
 import { ZodError } from "zod";
 import { IntegrationConfiguration } from "./integration-config.types";
 
+// Try to fetch config from environment and fail if we can't.
+function getOrThrow<T>(configService: ConfigService, key: string): T | undefined {
+  const value = configService.get<T>(key);
+
+  if (value === undefined) {
+    throw new Error("Unable to read environment variable " + key);
+  }
+
+  return value;
+}
+
 @Injectable()
 export class IntegrationConfigService {
   configuration: IntegrationConfiguration;
 
   constructor(private configService: ConfigService) {
-    // Build a config object.
-    const unsafeConfig = {
-      displayApiEndpoint: this.configService.get("DISPLAY_API_ENDPOINT"),
+    // Build a config object, pulling in configuration from the environment
+    // without doing a validation at this point.
+    const partialConfig: Partial<IntegrationConfiguration> = {
+      displayApiEndpoint: getOrThrow(configService, "DISPLAY_API_ENDPOINT"),
     };
 
     // Then validate it or crash.
     try {
-      IntegrationConfiguration.parse(unsafeConfig);
-      this.configuration = unsafeConfig;
+      IntegrationConfiguration.parse(partialConfig);
+
+      // We have validated the config so it is no longer partial.
+      this.configuration = partialConfig as IntegrationConfiguration;
     } catch (error) {
       throw new Error("Unable to parse configuration: " + (error as ZodError).message);
     }
